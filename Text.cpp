bd36d1ed03c109fe95e859762d58006c6e711e6b
Masarap Mabuhay <masarapmabuhay@gmail.com>
2021-12-30 14:37:27 +0800

    Add files via upload

--- Text.cpp
+++ Text.cpp
@@ -699,10 +699,10 @@ printf(">>>iTextBackgroundWidthOffset: %i\n",iTextBackgroundWidthOffset);
 void Text::drawTextBackgroundWithTexture()
 {
  	//edited by Mike, 20211221; edited again by Mike, 20211222
-	drawTextBackgroundWithTextureTile(TEXT_UP_RIGHT_CORNER_TILE, 1*fGridSquareWidth,fMyWindowHeight-3*fGridSquareHeight);
+	drawTextBackgroundWithTextureTile(TEXT_UP_RIGHT_CORNER_TILE, 2*fGridSquareWidth,fMyWindowHeight-3*fGridSquareHeight);
 
 	//added by Mike, 20211218
-	drawTextBackgroundWithTextureTile(TEXT_LEFT_UP_CORNER_TILE, 1*fGridSquareWidth,fMyWindowHeight-1*fGridSquareHeight);	
+	drawTextBackgroundWithTextureTile(TEXT_LEFT_UP_CORNER_TILE, 2*fGridSquareWidth,fMyWindowHeight-1*fGridSquareHeight);	
 
 	int iMaxColumnCountBasedOnWidth = fMyWindowWidth/fGridSquareWidth;
 
@@ -711,13 +711,15 @@ printf(">>>iTextBackgroundWidthOffset: %i\n",iTextBackgroundWidthOffset);
 	//edited by Mike, 20211230
 //	for (int iCount=3; iCount<iMaxColumnCountBasedOnWidth-3; iCount++) {
 //	for (int iCount=3; iCount<iMaxColumnCountBasedOnWidth-(iTextBackgroundWidthOffset-1); iCount++) {
-	for (int iCount=2; iCount<iMaxColumnCountBasedOnWidth-(iTextBackgroundWidthOffset-2); iCount++) {
+//	for (int iCount=2; iCount<iMaxColumnCountBasedOnWidth-(iTextBackgroundWidthOffset-2); iCount++) {
+	for (int iCount=3; iCount<iMaxColumnCountBasedOnWidth-3; iCount++) {
 		drawTextBackgroundWithTextureTile(TEXT_BOTTOM_SIDE_TILE, (iCount)*fGridSquareWidth,fMyWindowHeight-1*fGridSquareHeight);
 	}
 	
 	//added by Mike, 20211230
 //	drawTextBackgroundWithTextureTile(TEXT_DOWN_LEFT_CORNER_TILE, (iMaxColumnCountBasedOnWidth-(iTextBackgroundWidthOffset-1))*fGridSquareWidth,fMyWindowHeight-1*fGridSquareHeight);	
-	drawTextBackgroundWithTextureTile(TEXT_DOWN_LEFT_CORNER_TILE, (iMaxColumnCountBasedOnWidth-(iTextBackgroundWidthOffset-2))*fGridSquareWidth,fMyWindowHeight-1*fGridSquareHeight);	
+//	drawTextBackgroundWithTextureTile(TEXT_DOWN_LEFT_CORNER_TILE, (iMaxColumnCountBasedOnWidth-(iTextBackgroundWidthOffset-2))*fGridSquareWidth,fMyWindowHeight-1*fGridSquareHeight);	
+	drawTextBackgroundWithTextureTile(TEXT_DOWN_LEFT_CORNER_TILE, (iMaxColumnCountBasedOnWidth-3)*fGridSquareWidth,fMyWindowHeight-1*fGridSquareHeight);	
 
 /*
 	//edited by Mike, 20211222
